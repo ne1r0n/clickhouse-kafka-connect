2a9b289eb6cae1d0c325ec8e4141eabf57aa7871
Alexander Chashnikov <6350825+ne1r0n@users.noreply.github.com>
2024-12-23 12:38:31 +0200

    fix

--- src/main/java/com/clickhouse/kafka/connect/transforms/HeaderToValue.java
+++ src/main/java/com/clickhouse/kafka/connect/transforms/HeaderToValue.java
@@ -5,12 +5,12 @@ import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.header.Header;
 import org.apache.kafka.connect.transforms.Transformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
 public class HeaderToValue<R extends ConnectRecord<R>> implements Transformation<R> {
@@ -26,7 +26,6 @@ public class HeaderToValue<R extends ConnectRecord<R>> implements Transformation
 
     @Override
     public R apply(R record) {
-        LOGGER.debug("Old Headers: {}, Old Value: {}", record.headers(), record.value());
         Header header = record.headers().lastWithName(config.headerName());
 
         if (header == null || header.value() == null) {
@@ -53,7 +52,6 @@ public class HeaderToValue<R extends ConnectRecord<R>> implements Transformation
 
         final Map<String, Object> value = (Map<String, Object>) record.value();
         value.put(config.fieldName(), headerStringValue);
-        LOGGER.debug("New schemaless value: {}", value);
         return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), record.valueSchema(), value, record.timestamp());
     }
 
@@ -61,25 +59,30 @@ public class HeaderToValue<R extends ConnectRecord<R>> implements Transformation
         final Struct oldValue = (Struct) record.value();
 
         if (valueSchema == null) {
-            final SchemaBuilder builder = SchemaBuilder.struct();
-            builder.name(oldValue.schema().name());
-            builder.version(oldValue.schema().version());
-            builder.doc(oldValue.schema().doc());
-            oldValue.schema().fields().forEach(f -> builder.field(f.name(), f.schema()));
-            builder.field(config.fieldName(), Schema.STRING_SCHEMA);
-            valueSchema = builder.build();
+            valueSchema = buildSchema(oldValue.schema());
         }
 
         Struct newValue = new Struct(valueSchema);
-        valueSchema.fields().forEach(f -> {
-            if (f.name().equals(config.fieldName())) {
-                newValue.put(f, headerStringValue);
+        for (Field field : valueSchema.fields()) {
+            if (field.name().equals(config.fieldName())) {
+                newValue.put(field, headerStringValue);
             } else {
-                newValue.put(f, oldValue.get(f));
+                newValue.put(field, oldValue.get(field));
             }
-        });
-        LOGGER.debug("New schema value: {}", newValue);
+        }
         return record.newRecord(record.topic(), record.kafkaPartition(), record.keySchema(), record.key(), valueSchema, newValue, record.timestamp());
+    }
+
+    private Schema buildSchema(Schema oldSchema) {
+        SchemaBuilder builder = SchemaBuilder.struct();
+        builder.name(oldSchema.name());
+        builder.version(oldSchema.version());
+        builder.doc(oldSchema.doc());
+        for (Field field : oldSchema.fields()) {
+            builder.field(field.name(), field.schema());
+        }
+        builder.field(config.fieldName(), Schema.STRING_SCHEMA);
+        return builder.build();
     }
 
     @Override
